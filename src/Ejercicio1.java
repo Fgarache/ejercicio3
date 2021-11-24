2ab1ebca9880f93eb5128d079eb7b79be08fc63b
Francisco Garache <franciscogarach3@gmail.com>
2021-11-24 15:10:07 -0600

    nuevos  cambios

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -3,18 +3,29 @@ import java.util.Scanner;
 public class Ejercicio1 {
     public static void main(String[] args) {
        float num1;
-        Double num2;
-        int suma, resta, mult, div;
+        float num2;
+        int suma, resta, mult, div,obcion,respuesta;
 
         Scanner datos = new Scanner(System.in);
         System.out.println("SELECCIONE: \n1: SUMA, 2: RESTA, 3: MULTIPLICACION, 4 PARA DIVICION. ");
+        obcion = datos.nextInt();
 
-   switch (1==suma);
-   case 1 {
-                    System.out.println("Ingrese el numero 1: ");
-                    num1= datos.nextFloat();
-
+         if (obcion==1){
+         System.out.println("Ingrese le numero 1: ");
+                num1= datos.nextFloat();
+                System.out.println("Ingrese le numero 2: ");
+                num2= datos.nextFloat();
+                respuesta= (int) (num1+num2);
+          System.out.println("La suma es "+respuesta+"");
    }
+        if (obcion==2){
+        System.out.println("Ingrese le numero 1: ");
+            num1= datos.nextFloat();
+            System.out.println("Ingrese le numero 2: ");
+            num2= datos.nextFloat();
+            respuesta= (int) (num1-num2);
+        System.out.println("La resta es "+respuesta+"");
+}
 
     }
 }
